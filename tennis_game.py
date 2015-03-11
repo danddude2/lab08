488a1c891590122b2a4ea41974149a457ae83326
EricLindemann <elindemann3@gmail.com>
2015-03-11 10:26:57 -0600

    Initial Commit

---
+++ tennis_game.py
@@ -0,0 +1,22 @@
+class TennisGame:
+    def __init__(self):
+        """
+        This is the constructor for the TennisGame class. 
+        You may need to write code here such as declaring some fields 
+        to help with making your tests pass.
+        """
+        None
+
+    def score(self):
+        return ""
+
+    def playerOneScored(self):
+        None
+
+    def playerTwoScored(self):
+        None
+
+    """
+    It should not be necessary to create any other methods to complete the assignment.
+    If you feel like creating some helper methods you are welcome to.
+    """
---
+++ test_tennis_match.py
@@ -0,0 +1,13 @@
+from tennis_game import TennisGame
+import unittest
+
+class TestTennisGame(unittest.TestCase):
+
+    def testScore_ZeroToZero(self):
+        '''simple example to start you off'''
+        match = TennisGame()
+        self.assertEqual("love - love", match.score())
+
+
+if __name__ == '__main__':
+    unittest.main()
